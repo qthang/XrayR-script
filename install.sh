bc653d3b7ea5182de92b6beb5d255495ff9975de
qthang <quangthangdk95@gmail.com>
2022-05-06 10:52:20 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -115,14 +115,14 @@ install_XrayR() {
             exit 1
         fi
         echo -e "检测到 XrayR 最新版本：${last_version} - v0.8.0，开始安装"
-        wget -q -N --no-check-certificate -O /usr/local/XrayR/XrayR-linux.zip https://github.com/Misaka-blog/XrayR/releases/download/v0.8.0/XrayR-linux-${arch}.zip
+        wget -q -N --no-check-certificate -O /usr/local/XrayR/XrayR-linux.zip https://github.com/qthang/XrayR-Viet-release/releases/download/v0.8.0/XrayR-linux-${arch}.zip
         if [[ $? -ne 0 ]]; then
             echo -e "${red}下载 XrayR 失败，请确保你的服务器能够下载 Github 的文件${plain}"
             exit 1
         fi
     else
         last_version=$1
-        url="https://github.com/Misaka-blog/XrayR/releases/download/v0.8.0/XrayR-linux-${arch}.zip"
+        url="https://github.com/qthang/XrayR-Viet-release/releases/download/v0.8.0/XrayR-linux-${arch}.zip"
         echo -e "开始安装 XrayR v$1"
         wget -q -N --no-check-certificate -O /usr/local/XrayR/XrayR-linux.zip ${url}
         if [[ $? -ne 0 ]]; then
